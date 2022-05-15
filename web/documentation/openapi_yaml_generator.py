8cb1ed92ed0f81e1ca2de66c5bc7556497ee2287
Dmitrii Chebotarev <skrubberqumo@gmail.com>
2022-05-15 17:21:55 +0300

    feat(web.documentation): add custom openapi documentation generator

---
+++ web/documentation/openapi_yaml_generator.py
@@ -0,0 +1,15 @@
+import yaml
+from app.main import app
+
+
+def generate_documentation() -> None:
+    """Generates custom openapi.yaml SWAGGER documentation from fastapi app.
+
+    :return: None.
+    """
+    with open("./documentation/openapi.yaml", "w") as openapi_file:
+        yaml.dump(app.openapi(), openapi_file, sort_keys=False)
+
+
+if __name__ == "__main__":
+    generate_documentation()
